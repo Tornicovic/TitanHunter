54b3526a0714720b61f321b9ca3ebc8d63a0c964
Tornicovic <nicolas2008.la@gmail.com>
2026-07-13 23:33:13 +0200

    Primera versión del juego

---
+++ game.js
@@ -0,0 +1,15 @@
+const playBtn=document.getElementById("playBtn");
+
+const optionsBtn=document.getElementById("optionsBtn");
+
+playBtn.onclick=function(){
+
+alert("Bienvenido.\nEn la siguiente versión añadiremos el tutorial.");
+
+}
+
+optionsBtn.onclick=function(){
+
+alert("Opciones disponibles próximamente.");
+
+}
